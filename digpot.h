197b173288d9c3ef7d2437f08e44fba0ebe9e9f8
saroe1 <sar1295@gmail.com>
2015-02-24 13:53:41 -0600

    Modified DigPot.h
    Updated DigPot to contain all necessary OP Commands and addresses.

--- digpot.h
+++ digpot.h
@@ -10,10 +10,28 @@
 
 #ifndef DIGPOT_H_
 #define DIGPOT_H_
-typedef unsigned char uint8_t;
-typedef unsigned char uint16_t;
+#include <stdint.h>
 
-//#define DigPot_Address
+//Slave Address
+#define staticAddr 0x05
+
+//Selects pin A1 as high, must be pulled high, and all other pins (A0, A2, A3) pulled low.
+#define AddrSelect 0x02
+
+//OP Commands
+#define ReadWCRCmd 0x90
+#define WriteWCRCmd 0xa0
+#define ReadDRCmd 0xb0
+#define WriteDRCmd 0xc0
+#define XFR_DR_to_WCR 0xd0
+#define XFR_WCR_to_DR 0xe0
+#define GangXFR_DR_to_WCR 0x01
+#define GangXFR_WCR_to_DR 0x02
+#define Inc_Dec_Enable 0x02
+
+//Potentiometer and Data Register Address
+#define DataRegister 0x00
+#define Pot_0 0x00
 
 
 //Function prototypes
@@ -27,5 +45,7 @@ void DigPot_Decrement();												//Decrements wiper value by one step (one st
 uint8_t testConnect();													//Tests connection to Digital Potentiometer
 void DigPot_init();														//Initiates the Digital Potentiometer, sets value of wiper to lowest (50k)
 
+void TransferWCR_to_DR();												//Transfers WCR data to Data Register
+void TransferDR_to_WCR();												//Transfers Data Register Data to WCR
 
 #endif /* DIGPOT_H_ */
