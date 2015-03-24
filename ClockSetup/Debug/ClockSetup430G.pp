5f47481f2b9e7c79be4e2a6cc6b362e1d5037530
Sebastian A Roe <sar12795@gmail.com>
2015-03-24 16:47:17 -0500

    Created a new main.c to control MSP420G5223

---
+++ ClockSetup/Debug/ClockSetup430G.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+ClockSetup430G.obj: ../ClockSetup430G.c
+ClockSetup430G.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+ClockSetup430G.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h
+ClockSetup430G.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+ClockSetup430G.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+ClockSetup430G.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+
+../ClockSetup430G.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5969.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
